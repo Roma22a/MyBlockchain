8de82be4d31ebe6d97d3b30c6581119b6a064f97
Roma22a <roma22auser@gmail.com>
2020-05-27 13:50:02 -0400

    Add routes

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 
 from time import time
 from uuid import uuid4
+from textwrap import dedent
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
@@ -94,3 +96,27 @@ class Blockchain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return 'Mine a new Block'
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return 'Add a new transaction'
+
+@app.route('chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
